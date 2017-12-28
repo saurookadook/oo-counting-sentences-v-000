5fc7c7f4ac7021c68f579f95725a02910d7f5195
Andy Maskiell <maskiella@gmail.com>
2017-12-28 19:16:28 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.split(".")
   end
 
   def question?
-
+    self.split("?")
   end
 
   def exclamation?
-
+    self.split(!)
   end
 
   def count_sentences
-
+    binding.pry
   end
 end
